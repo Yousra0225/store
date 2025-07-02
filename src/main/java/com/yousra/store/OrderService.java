0d4b6cedeecb7289eaf388a0f43cf96ed4f661ff
Yousra0225 <chbibyousra02@gmail.com>
2025-07-02 23:08:14 +0200

    feat(order-service): add order service class

--- src/main/java/com/yousra/store/OrderService.java
+++ src/main/java/com/yousra/store/OrderService.java
@@ -1,4 +1,8 @@
 package com.yousra.store;
 
 public class OrderService {
+    public void placeOrder(){
+        var paymentService = new StripePaymentService();
+        paymentService.processPayment(10);
+    }
 }
