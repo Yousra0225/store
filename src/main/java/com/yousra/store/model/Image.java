8c59cc93dd6605bb96a3f399ab0f706e43cbb484
Yousra0225 <chbibyousra02@gmail.com>
2025-07-04 23:17:28 +0200

    feat(image): add tostring method

--- src/main/java/com/yousra/store/model/Image.java
+++ src/main/java/com/yousra/store/model/Image.java
@@ -1,21 +1,48 @@
 package com.yousra.store.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.*;
 
 import java.util.UUID;
 
+@Entity
 public class Image {
 
-    @id
-    @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
     @Column
     private String url;
 
+    @ManyToOne
+    @JoinColumn(name = "article_id", nullable = false)
+    private Article article;
+    /**
+     * Constructor
+     */
     public Image(){}
 
-    
+    /**
+     * Getters
+     */
+    public UUID getId() {return this.id;}
+    public String getUrl(){return this.url;}
+    public Article getArticle() {return this.article;}
+    /**
+     * Setters
+     */
+    public void setId(UUID id){this.id = id;}
+    public void setUrl(String url){this.url = url;}
+    public void setArticle(Article article) {this.article = article;}
 
+    /**
+     * to string method
+     * @return string
+     */
+    public String toString() {
+        return "Image{" +
+                "id=" + id +
+                ", url='" + url + '\'' +
+                '}';
+    }
 }
