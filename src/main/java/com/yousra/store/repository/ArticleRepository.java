6169c66b6f98de168d820d56a6a3bf5bef06efe9
Yousra0225 <chbibyousra02@gmail.com>
2025-07-07 23:57:17 +0200

    feat(article-repository): update

--- src/main/java/com/yousra/store/repository/ArticleRepository.java
+++ src/main/java/com/yousra/store/repository/ArticleRepository.java
@@ -2,6 +2,7 @@ package com.yousra.store.repository;
 
 import com.yousra.store.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends JpaRepository<Article, UUID> {
 }
