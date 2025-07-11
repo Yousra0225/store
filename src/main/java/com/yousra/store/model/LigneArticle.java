1309cc46a9bb83b0ea00fd61f5311dd6703dcd82
Yousra0225 <chbibyousra02@gmail.com>
2025-07-11 22:23:59 +0200

    feat(article-ligne): add ligne article class

--- src/main/java/com/yousra/store/model/LigneArticle.java
+++ src/main/java/com/yousra/store/model/LigneArticle.java
@@ -1,4 +1,33 @@
-package com.yousra.store.model;
+package com.ta_boutique.model;
 
+import com.yousra.store.model.Article;
+import com.yousra.store.model.Command;
+import com.yousra.store.model.Panier;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
 public class LigneArticle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @ManyToOne
+    private Article article;
+
+    private int quantite;
+
+    private BigDecimal prixUnitaire;
+
+    @ManyToOne
+    private Panier panier;
+
+    @ManyToOne
+    private Command commande;
+
+    
+
+
 }
